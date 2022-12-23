4081ed2980cdbd2c5219a26485c501a56e38478e
Юля Кобзева <selemina@mail.ru>
2022-12-23 15:12:01 +0300

    дз

--- src/test/java/sky/pro/calculator/service/CalculatorServiceTest.java
+++ src/test/java/sky/pro/calculator/service/CalculatorServiceTest.java
@@ -6,34 +6,34 @@ import org.junit.jupiter.api.Test;
 public class CalculatorServiceTest {
     private final Integer ILLEGAL_ARGUMENT = 0;
     private final Integer NUM1 = 1;
-    private final Integer NUM2 = 2;
+    private final Integer NUM2 = 5;
     private final CalculatorService calculatorService = new CalculatorService();
 
     @Test
     public void shouldNumsEqualsWhenPlus(){
-        Integer expected = calculatorService.plus(NUM1,NUM2);
-        Integer actual = NUM1 + NUM1;
+        Integer expected = calculatorService.plus(NUM1, NUM2);
+        Integer actual = NUM1 + NUM2;
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
     public void shouldNumsEqualsWhenMinus(){
-        Integer expected = calculatorService.minus(NUM1,NUM2);
-        Integer actual = NUM1 - NUM1;
+        Integer expected = calculatorService.minus(NUM1, NUM2);
+        Integer actual = NUM1 - NUM2;
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
     public void shouldNumsEqualsWhenMultiply(){
-        Integer expected = calculatorService.multiply(NUM1,NUM2);
-        Integer actual = NUM1 * NUM1;
+        Integer expected = calculatorService.multiply(NUM1, NUM2);
+        Integer actual = NUM1 * NUM2;
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
     public void shouldNumsEqualsWhenDevide(){
-        Integer expected = calculatorService.divide(NUM1,NUM2);
-        Integer actual = NUM1 / NUM1;
+        Integer expected = calculatorService.divide(NUM1, NUM2);
+        Integer actual = NUM1 / NUM2;
         Assertions.assertEquals(expected,actual);
     }
 
